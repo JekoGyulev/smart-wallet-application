01907726f0caa31cca7fc0c30b7826b652c2b91d
JekoGyulev <veracvetanova7@gmail.com>
2025-10-23 22:10:30 +0300

    refactor: make EventListeners asynchronous

--- src/main/java/app/email/EmailService.java
+++ src/main/java/app/email/EmailService.java
@@ -3,12 +3,14 @@ package app.email;
 import app.event.SuccessfulChargeEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
     @EventListener
+    @Async
     @Order(1)
     public void sendEmailWhenChargeHappens(SuccessfulChargeEvent successfulChargeEvent) {
         System.out.printf("Sending email for new payment happened for user with email [%s]"
--- src/main/java/app/gift/GiftService.java
+++ src/main/java/app/gift/GiftService.java
@@ -4,12 +4,14 @@ package app.gift;
 import app.event.SuccessfulChargeEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GiftService {
 
     @EventListener
+    @Async
     @Order(2)
     public void sendGift(SuccessfulChargeEvent  successfulChargeEvent) {
         System.out.printf("Sending 1 Euro for charge compensation for user with email [%s]."
