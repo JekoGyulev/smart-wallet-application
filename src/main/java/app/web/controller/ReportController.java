bd90a29f65d0f4c8e48a50566ddc0040f0c6abd1
JekoGyulev <veracvetanova7@gmail.com>
2025-10-26 15:30:45 +0200

    feat: show admin-only pages to users with role ADMIN

--- src/main/java/app/web/controller/ReportController.java
+++ src/main/java/app/web/controller/ReportController.java
@@ -6,6 +6,7 @@ import app.user.model.User;
 import app.user.service.UserService;
 import app.wallet.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,7 @@ public class ReportController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ModelAndView getReportsPage() {
         Map<String, Object> stats = getStats();
 
